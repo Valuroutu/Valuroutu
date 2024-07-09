5f2abc2138c6c5f09c741eac420cd785502b9ff5
Valuroutu <167420939+Valuroutu@users.noreply.github.com>
2024-07-09 16:25:57 +0530

    Add files via upload

---
+++ one.py
@@ -0,0 +1,17 @@
+import random
+import calendar
+list=[1,2,3,4,56,7]
+list.sort()
+print(list)
+for x in list:
+    print("santosh")
+if 22 in list:
+    print("yes")
+else:
+    print("no")
+print(random.randrange(1,20))
+print(calendar.month(2021,5))
+for i in range(6):
+    for j in range(6-i):
+        print(i,end=" ")
+    print(" ")
